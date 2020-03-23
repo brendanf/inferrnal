c56ca9bbe2c13237ed129f0781a3c39eb71a7b51
brendanf <brendan.furneaux@gmail.com>
2020-03-23 13:41:39 +0100

    Really add the test

--- tests/testthat/test-read_stockholm_msa.R
+++ tests/testthat/test-read_stockholm_msa.R
@@ -1,3 +1,20 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("fails on integer", {
+  expect_error(read_stockholm_msa(1L))
+})
+
+test_that("fails on double", {
+    expect_error(read_stockholm_msa(1))
+})
+
+test_that("fails on logical", {
+    expect_error(read_stockholm_msa(FALSE))
+})
+
+test_that("fails on nonfile", {
+    expect_error(read_stockholm_msa(tempfile("fake")))
+})
+
+
+test_that("can read file", {
+    expect_known_value(read_stockholm_msa(system.file(file.path("extdata", "sample.stk"), package = "inferrnal")), file = here::here(file.path("tests", "testthat", "read_stockholm_msa_out")))
 })
