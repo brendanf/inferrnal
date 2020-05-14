75318812ffc8508e5a6c5d3d7f5ecb94f27eeaec
brendanf <brendan.furneaux@gmail.com>
2020-05-15 00:52:47 +0200

    use 5.8S alignment to test cmbuild()

--- tests/testthat/test-cmbuild.R
+++ tests/testthat/test-cmbuild.R
@@ -3,22 +3,16 @@ test_cm <- tempfile("test", fileext = ".cm")
 test_that("cmbuild can replicate RDP CM", {
     expect_null(
         cmbuild(
-            msafile = system.file(
-                file.path("extdata", "fungi_32S_LR5.stk"), package = "LSUx"
-            ),
+            msafile = stk_5_8S(),
             cmfile_out = test_cm,
             force = TRUE,
-            consensus_method = "hand",
-            ere = 0.85,
-            extra = "--iflank",
             verbose = TRUE)
     )
     # these lines have timestamps and system paths that are not expected to
-    # replicate
+    # replicate, or are for the calibrated cm.
+    # also one line has a deviation in the least significant digit.
   expect_identical(
-      readLines(
-          system.file(file.path("extdata", "fungi_32S_LR5.cm"), package = "LSUx")
-      )[-c(2, 11, 12, 4328, 4337, 4338)],
-      readLines(test_cm)[-c(2, 11, 12, 4328, 4337, 4338)]
+      readLines(cm_5_8S())[-c(1, 13, 14, 15, 32:35, 662, 665, 674:676, 913)],
+      readLines(test_cm)[-c(1, 13, 14, 657, 660, 669:670, 907)]
   )
 })
