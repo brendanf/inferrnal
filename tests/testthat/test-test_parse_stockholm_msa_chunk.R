c28229dd91306506520bd406cac17aa3f34e9a0a
brendanf <brendan.furneaux@gmail.com>
2020-06-14 15:35:56 +0200

    update expected test results for alignments

--- tests/testthat/test-test_parse_stockholm_msa_chunk.R
+++ tests/testthat/test-test_parse_stockholm_msa_chunk.R
@@ -7,20 +7,20 @@ stockholm_test <- c(
 test_that("parse_stockholm_msa_chunk works for a sequence", {
     expect_equal(
         inferrnal:::parse_stockholm_msa_chunk(stockholm_test[1], pos = 1, acc = list()),
-        list(testtaxon = "AGCG--AGC")
+        list(alignment = c(testtaxon = "AGCG--AGC"))
     )
 })
 
 test_that("parse_stockholm_msa_chunk works for a GC line", {
     expect_equal(
         inferrnal:::parse_stockholm_msa_chunk(stockholm_test[2], pos = 1, acc = list()),
-        structure(list(), test = "-(((..)))")
+        list(GC = c(test = "-(((..)))"))
     )
 })
 
 test_that("parse_stockholm_msa_chunk works for a sequence + GC line", {
     expect_equal(
         inferrnal:::parse_stockholm_msa_chunk(stockholm_test, pos = 1, acc = list()),
-        structure(list(testtaxon = "AGCG--AGC"), test = "-(((..)))")
+        list( GC = c(test = "-(((..)))"), alignment = c(testtaxon = "AGCG--AGC"))
     )
 })
