6e45eb40164f6792e00d992ba61c622dd73d9aba
brendanf <brendan.furneaux@gmail.com>
2020-11-05 16:46:03 +0100

    skip comparison with RFAM CM if Infernal version is greater than 1.1.3, because it will no longer match.

--- tests/testthat/test-cmbuild.R
+++ tests/testthat/test-cmbuild.R
@@ -11,8 +11,15 @@ test_that("cmbuild can replicate RDP CM", {
     # these lines have timestamps and system paths that are not expected to
     # replicate, or are for the calibrated cm.
     # also one line has a deviation in the least significant digit.
-  expect_identical(
-      readLines(cm_5_8S())[-c(1, 13, 14, 15, 32:35, 662, 665, 674:676, 913)],
-      readLines(test_cm)[-c(1, 13, 14, 657, 660, 669:670, 907)]
-  )
+    infernal_ver <- gsub(
+        "# INFERNAL ([0-9.]+).+",
+        "\\1",
+        system2("cmbuild", "-h", stdout = TRUE)[2]
+    )
+    skip_if(utils::compareVersion(infernal_ver,"1.1.3") >= 0,
+            "skipping comparison with RFAM CM due to changes in Infernal 1.1.3")
+    expect_identical(
+        readLines(cm_5_8S())[-c(1, 13, 14, 15, 32:35, 662, 665, 674:676, 913)],
+        readLines(test_cm)[-c(1, 13, 14, 657, 660, 669:670, 907)]
+    )
 })
